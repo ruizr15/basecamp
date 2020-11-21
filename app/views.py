a5679e10e7a756bf29562f4395d2bc75ab18690b
Scooney6 <cooney.samuel@gmail.com>
2020-11-21 15:35:27 -0500

    Added basic NPS API setup per state

--- app/views.py
+++ app/views.py
@@ -1,9 +1,21 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import json, requests
 
-@app.route("/")
+
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        state=request.form["state"]
+        if state != "":
+            endpoint = "https://developer.nps.gov/api/v1/campgrounds?stateCode="+ str(state) + "&api_key=***REMOVED***"
+            HEADERS = {"Authorization": "***REMOVED***"}
+            req = requests.get(endpoint)
+            return render_template("index.html", info=req.text)
+        else:
+            return render_template("index.html", info="Input your state to begin")
+    else:
+        return render_template("index.html", info="Input your state to begin")
 
 @app.route("/login")
 def login():
