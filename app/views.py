46a97a747d3050780ea3f6460d1cd36108006526
ruizr15 <71466308+ruizr15@users.noreply.github.com>
2020-11-21 02:45:29 -0500

    made a site to gather coords

--- app/views.py
+++ app/views.py
@@ -1,10 +1,31 @@
 from app import app
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash, jsonify
+import requests
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return redirect("/coordinates")
+    # return render_template("index.html")
 
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/coordinates", methods=["GET", "POST"])
+def coordinates():
+    return render_template("coordinates.html")
+
+@app.route("/weather", methods=["GET", "POST"])
+def weather():
+    if request.method == "POST":
+        lat = request.form["latitude"]
+        lon = request.form["longitude"]
+        r = requests.get('https://api.weather.gov/points/32.8427,-83.9577')
+        return jsonify(r.json())
+
+
+@app.route("/get", methods=["POST"])
+def test():
+    r = requests.get('https://api.weather.gov/points/32.8427,-83.9577')
+    return jsonify(r.json())
+
